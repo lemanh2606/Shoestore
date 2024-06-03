3d65350fb15ccdd7aef947bc13f29589c9802aa6
DESKTOP-1NLGI83\MSI <nguyendinhlinh19062003@gmail.com>
2024-06-03 17:10:55 +0700

    Add Category

---
+++ Category/UpdateCategoryServlet.java
@@ -0,0 +1,106 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+package Category;
+
+import Dal.CategoryDAO;
+import Model.Category;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author MSI
+ */
+public class UpdateCategoryServlet extends HttpServlet {
+   
+    /** 
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet UpdateCategoryServlet</title>");  
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet UpdateCategoryServlet at " + request.getContextPath () + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    } 
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /** 
+     * Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        String id_raw = request.getParameter("categoryID");
+        int id;
+        CategoryDAO d = new CategoryDAO();
+        try {
+            id = Integer.parseInt(id_raw);
+            Category c = d.getCategoryByID(id);
+            request.setAttribute("cate", c);
+            request.getRequestDispatcher("updatecategory.jsp").forward(request, response);
+        } catch (NumberFormatException e) {
+            System.out.println(e);
+        }
+    } 
+
+    /** 
+     * Handles the HTTP <code>POST</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        String id_raw = request.getParameter("categoryID");
+        String name = request.getParameter("categoryName");
+        String description = request.getParameter("description");
+        int id;
+        CategoryDAO d = new CategoryDAO();
+        try {
+            id = Integer.parseInt(id_raw);
+            Category c = new Category(id, name, description, name);
+            d.updateCategory(c);
+        } catch (NumberFormatException e) {
+            System.out.println(e);
+        }
+        response.sendRedirect("categorylist"); // Admin list server
+    }
+
+    /** 
+     * Returns a short description of the servlet.
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
