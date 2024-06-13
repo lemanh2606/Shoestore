c750dcc7facf4cc10d6a8b83e0378be426b7ce0e
DESKTOP-1NLGI83\MSI <nguyendinhlinh19062003@gmail.com>
2024-06-13 22:03:38 +0700

    Update project

---
+++ SWP391-Shoestore/SWP391-Shoestore/src/java/DBContext/NotificationDAO.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DBContext;
+
+import entity.Notification;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class NotificationDAO extends DBcontext{
+   
+    public void addNotification(Notification notification)  {
+        try {
+          String  query = "INSERT INTO dbo.[Notifications] VALUES (?,?,?,0,GETDATE())";
+            
+         PreparedStatement   ps = connection.prepareStatement(query);
+            ps.setInt(1, notification.getUserID());
+            ps.setInt(2,notification.getOrderID());
+            ps.setNString(3, notification.getContent());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            Logger.getLogger(NotificationDAO.class.getName()).log(Level.SEVERE, null, e);
+        }
+        
+    }
+    public ArrayList<Notification> getAllNotification(int id) {
+        ArrayList<Notification> list = new ArrayList<>();
+        try {
+         String   query = "SELECT * FROM dbo.[Notifications] WHERE UserID = ? ORDER BY [status] ASC, [time] DESC";
+          
+          PreparedStatement  ps = connection.prepareStatement(query);
+            ps.setInt(1, id);
+           ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                list.add(new Notification(rs.getInt("ID"),rs.getInt("UserID"), rs.getInt("OrderID"), rs.getString("Content"), rs.getInt("status"), rs.getDate("time")));
+            }
+        } catch (SQLException e) {
+            Logger.getLogger(NotificationDAO.class.getName()).log(Level.SEVERE, null, e);
+        }
+       
+        return list;
+    }
+    public void markAsRead(int id,int notiId){
+        try {
+         String   query = "UPDATE dbo.Notifications set status=1 where UserID = ? and ID = ?";
+           
+          PreparedStatement  ps = connection.prepareStatement(query);
+            ps.setInt(1, id);
+            ps.setInt(2, notiId);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            Logger.getLogger(NotificationDAO.class.getName()).log(Level.SEVERE, null, e);
+        }
+       
+    }
+    public void readAll(int id){
+        try {
+          String  query = "UPDATE dbo.Notifications set status=1 where UserID = ?";
+        
+       PreparedStatement     ps = connection.prepareStatement(query);
+            ps.setInt(1, id);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            Logger.getLogger(NotificationDAO.class.getName()).log(Level.SEVERE, null, e);
+        }
+       
+    }
+}
---
+++ SWP391-Shoestore/SWP391-Shoestore/src/java/DBContext/TrendDAO.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DBContext;
+
+
+import entity.Trend;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class TrendDAO extends DBcontext{
+ 
+    public List<Trend> getCategoryTrend(Date start,Date end){
+        int count=0;
+        int sum=0;
+        SimpleDateFormat SDF= new SimpleDateFormat("yyyy-MM-dd");
+        List<Trend> list=new ArrayList<>();
+        try {
+       String     query = "SELECT odr.SubCategoryID,SubCategoryName,SUM(odr.Quantity) AS Count FROM ( SELECT od.ProductID,od.ProductName,od.Quantity,od.Date,SubCategoryID,BrandID FROM (SELECT ProductID,ProductName,Quantity,Date FROM  dbo.Orders JOIN dbo.Order_Detail  ON Order_Detail.Order_ID = Orders.ID ) od JOIN dbo.Product ON Product.ProductID = od.ProductID WHERE od.Date BETWEEN CAST( ? AS DATE) AND CAST( ? AS DATE)  ) odr JOIN dbo.SubCategory ON SubCategory.SubCategoryID = odr.SubCategoryID GROUP BY odr.SubCategoryID,SubCategoryName ORDER BY COUNT DESC";
+         
+            PreparedStatement ps = connection.prepareStatement(query);
+            ps.setString(1,SDF.format(start) );
+            ps.setString(2, SDF.format(end));
+         ResultSet   rs = ps.executeQuery();
+            while (rs.next()) {
+                if(count>3)
+                    sum+=rs.getInt("Count");
+                else
+                    list.add( new Trend(rs.getInt("SubCategoryID"),rs.getString("SubCategoryName"),rs.getInt("Count")) );
+                count++;
+            }
+        } catch (SQLException e) {
+            Logger.getLogger(TrendDAO.class.getName()).log(Level.SEVERE, null, e);
+        }
+       
+        if(count>3)
+            list.add(new Trend(0,"Other",sum));
+        return list;
+    }
+    public List<Trend> getProductTrend(Date start,Date end){
+        int count=0;
+        int sum=0;
+        SimpleDateFormat SDF= new SimpleDateFormat("yyyy-MM-dd");
+        List<Trend> list=new ArrayList<>();
+        try {
+           String query = "SELECT TOP 10 odr.ProductID,odr.ProductName,SUM(odr.Quantity) AS Count  FROM ( SELECT od.ProductID,od.ProductName,od.Quantity,od.Date,SubCategoryID,BrandID FROM  (SELECT ProductID,ProductName,Quantity,Date FROM  dbo.Orders JOIN dbo.Order_Detail  ON Order_Detail.Order_ID = Orders.ID ) od JOIN dbo.Product ON Product.ProductID = od.ProductID WHERE od.Date BETWEEN CAST( ? AS DATE) AND CAST( ? AS DATE) ) odr GROUP BY odr.ProductID, odr.ProductName ORDER BY COUNT DESC";
+           
+          PreparedStatement  ps = connection.prepareStatement(query);
+            ps.setString(1,SDF.format(start) );
+            ps.setString(2, SDF.format(end));
+            System.out.print(ps.toString());
+          ResultSet  rs = ps.executeQuery();
+            while (rs.next()) {
+                list.add( new Trend(rs.getInt("ProductID"),rs.getString("ProductName"),rs.getInt("Count")) );
+            }
+        } catch (SQLException e) {
+            Logger.getLogger(TrendDAO.class.getName()).log(Level.SEVERE, null, e);
+        }
+       
+        return list;
+    }
+    public List<Trend> getBrandTrend(Date start,Date end){
+        int count=0;
+        int sum=0;
+        SimpleDateFormat SDF= new SimpleDateFormat("yyyy-MM-dd");
+        List<Trend> list=new ArrayList<>();
+        try {
+         String   query = "SELECT odr.BrandID,BrandName,SUM(odr.Quantity) AS Count FROM (SELECT od.ProductID,od.ProductName,od.Quantity,od.Date,SubCategoryID,BrandID FROM (SELECT ProductID,ProductName,Quantity,Date FROM  dbo.Orders JOIN dbo.Order_Detail  ON Order_Detail.Order_ID = Orders.ID ) od JOIN dbo.Product ON Product.ProductID = od.ProductID WHERE od.Date BETWEEN CAST( ? AS DATE) AND CAST( ? AS DATE)  ) odr JOIN dbo.Brand ON Brand.BrandID = odr.BrandID GROUP BY odr.BrandID,BrandName ORDER BY COUNT DESC";
+            
+           PreparedStatement ps = connection.prepareStatement(query);
+            ps.setString(1,SDF.format(start) );
+            ps.setString(2, SDF.format(end));
+         ResultSet   rs = ps.executeQuery();
+            while (rs.next()) {
+                if(count>3)
+                    sum+=rs.getInt("Count");
+                else
+                    list.add( new Trend(rs.getInt("BrandID"),rs.getString("BrandName"),rs.getInt("Count")) );
+                count++;
+            }
+        } catch (SQLException e) {
+            Logger.getLogger(TrendDAO.class.getName()).log(Level.SEVERE, null, e);
+        }
+      
+        if(count>3)
+            list.add(new Trend(0,"Other",sum));
+        return list;
+    }
+}
